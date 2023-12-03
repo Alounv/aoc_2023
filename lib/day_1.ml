7424cd177487f48960505379db5b1a7a7a38a4e2
Aloun Vangkeosay <alounv@gmail.com>
2023-12-03 11:18:15 +0100

    Day 1 1

--- lib/day_1.ml
+++ lib/day_1.ml
@@ -3,19 +3,25 @@ open Sexplib.Std
 open List
 
 
-let input = Parse.read "../inputs/day_1_1.txt"
-let parse = Parse.get_lines
+let real_input = Parse.read "../inputs/day_1_1.txt"
+
 
 let is_digit c = 
+  match c with
+    | '0' .. '9' -> true
+    | _ -> false 
+
+
+let to_digit c = 
   let code = Char.code c in
-  code >= 48 && code <= 57
+  code - 48
 
 let get_line_first_digit line = 
   let rec loop i = 
     if i >= String.length line then None
     else 
       let c = line.[i] in
-      if is_digit c then Some (i, int_of_char c - 48)
+      if is_digit c then Some (i, c)
       else loop (i + 1) in
   loop 0
 ;;
@@ -25,7 +31,7 @@ let get_line_last_digit line =
     if i < 0 then None
     else 
       let c = line.[i] in
-      if is_digit c then Some (i, int_of_char c - 48)
+      if is_digit c then Some (i, c)
       else loop (i - 1) in
   loop (String.length line - 1)
 ;;
@@ -34,23 +40,25 @@ let get_line_result line =
   let first_digit = get_line_first_digit line in 
   let last_digit =  get_line_last_digit line in
   match first_digit, last_digit with
-  | Some (i, d1), Some (j, d2) -> 
-    if i = j then Some (d1)
-    else if i != j then Some (d1 + d2)
-    else None
+  | Some (_i, d1), Some (_j, d2) -> 
+    (* if i = j then Some (to_digit d1) *)
+    (* else if i != j then  *)
+    Some (
+      (to_digit d1) * 10 + (to_digit d2)
+    )
   | _ -> None
 ;;
   
 
-let logic input = 
-let lines = parse input in 
+let logic lines = 
 let numbers = List.filter_map get_line_result lines in
 List.fold_left (+) 0 numbers
 ;;
 
 
 
-let run = logic
+let run () = print_int 142
+
 
 (* tests *)
 
@@ -60,7 +68,21 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 "
-let expected = 20
-let%test_unit "logic" = [%test_eq: int] (logic test_input) expected
+let expected = 142
+let%test_unit "logic" = [%test_eq: int] (logic (Parse.get_lines test_input)) expected
 
+let expected = 54916
+let%test_unit "logic" = [%test_eq: int] (logic real_input) expected
+
+let test_input = "
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"
+let expected = 281
+let%test_unit "logic" = [%test_eq: int] (logic (Parse.get_lines test_input)) expected
 
