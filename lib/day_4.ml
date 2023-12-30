a597a75e13169e33dba6858c5ff7471cffbef45a
Alounv <34238160+Alounv@users.noreply.github.com>
2023-12-30 21:56:15 +0100

    Enable formatting

--- lib/day_4.ml
+++ lib/day_4.ml
@@ -2,89 +2,81 @@
 (* open Sexplib.Std *)
 open List
 
-type card = {
-  id: int;
-  winning: int list;
-  owned: int list;
-}
+type card = { id : int; winning : int list; owned : int list }
 
 let spaces_reg = Str.regexp "[ \t]+"
 
-let get_card (line: string): card = 
+let get_card (line : string) : card =
   let parts = String.split_on_char ':' line in
   let id = List.hd parts in
-  let id = Str.split (spaces_reg) (id) in
+  let id = Str.split spaces_reg id in
   let id = List.nth id 1 in
   let id = int_of_string id in
   let content = List.nth parts 1 in
 
   let parts = String.split_on_char '|' content in
-  let winning = Str.split (spaces_reg) (List.hd parts) in
-  let owned = Str.split (spaces_reg) (List.nth parts 1) in
+  let winning = Str.split spaces_reg (List.hd parts) in
+  let owned = Str.split spaces_reg (List.nth parts 1) in
   let winning = List.map int_of_string winning in
   let owned = List.map int_of_string owned in
   { id; winning; owned }
 
-let get_cards (lines: string list): card list =
-  let rec loop (lines: string list) (acc: card list): card list = 
+let get_cards (lines : string list) : card list =
+  let rec loop (lines : string list) (acc : card list) : card list =
     match lines with
     | [] -> acc
-    | line :: lines -> 
-      let card = get_card line in
-      loop lines (card :: acc)
+    | line :: lines ->
+        let card = get_card line in
+        loop lines (card :: acc)
   in
   loop lines []
 
-let card_win(card: card): int = 
+let card_win (card : card) : int =
   let rec count = function
     | [] -> 0
-    | x :: rest -> 
-      if List.mem x card.winning then 1 + count rest
-      else count rest
+    | x :: rest ->
+        if List.mem x card.winning then 1 + count rest else count rest
   in
-  count card.owned 
-;;
+  count card.owned
 
-let card_score(card: card): int = 
+let card_score (card : card) : int =
   let win = card_win card in
-  if (win <= 1) then win
+  if win <= 1 then win
   else
     let win = max (win - 1) 0 in
-    let score = 2.0 ** (float_of_int win) |> int_of_float in
+    let score = 2.0 ** float_of_int win |> int_of_float in
     score
-;;
 
-let logic (lines: string list): int  = 
+let logic (lines : string list) : int =
   let cards = get_cards lines in
-  let scores = List.map (card_score) cards in
-  List.fold_left (+) 0 scores
-;;
+  let scores = List.map card_score cards in
+  List.fold_left ( + ) 0 scores
 
-let get_won_cards (id: int) (win: int): int list = 
-  let rec loop (id: int) (win: int) (acc: int list): int list = 
-    if win = 0 then acc
-    else loop (id + 1) (win - 1) (id :: acc)
+let get_won_cards (id : int) (win : int) : int list =
+  let rec loop (id : int) (win : int) (acc : int list) : int list =
+    if win = 0 then acc else loop (id + 1) (win - 1) (id :: acc)
   in
   loop (id + 1) win []
-;;
 
-module M = Map.Make(Int)
+module M = Map.Make (Int)
 
-let increment_value (count_map: int M.t) (cardId: int) (increment: int): int M.t = 
+let increment_value (count_map : int M.t) (cardId : int) (increment : int) :
+    int M.t =
   let value = M.find_opt cardId count_map in
   match value with
   | None -> M.add cardId increment count_map
   | Some value -> M.add cardId (value + increment) count_map
 
-let rec increment_card_values (count_map: int M.t) (cardsIds: int list) (increment: int): int M.t = 
+let rec increment_card_values (count_map : int M.t) (cardsIds : int list)
+    (increment : int) : int M.t =
   match cardsIds with
   | [] -> count_map
-  | cardId :: rest -> 
-    let count_map = increment_value count_map cardId increment in
-    increment_card_values count_map rest increment
-;;
+  | cardId :: rest ->
+      let count_map = increment_value count_map cardId increment in
+      increment_card_values count_map rest increment
 
-let rec get_scratched_card (wins_map: int M.t) (count_map: int M.t) (index: int): int M.t = 
+let rec get_scratched_card (wins_map : int M.t) (count_map : int M.t)
+    (index : int) : int M.t =
   let cards_count = M.cardinal wins_map in
   if index > cards_count then count_map
   else
@@ -94,36 +86,33 @@ let rec get_scratched_card (wins_map: int M.t) (count_map: int M.t) (index: int)
     let won_cards = get_won_cards index wins in
     let count_map = increment_card_values count_map won_cards current_value in
     get_scratched_card wins_map count_map (index + 1)
-;;
 
-let logic2 (lines: string list): int  = 
+let logic2 (lines : string list) : int =
   let cards = get_cards lines in
   let cards_wins = List.map (fun card -> (card.id, card_win card)) cards in
-  let wins_map = List.fold_left (fun acc (id, win) -> M.add id win acc) M.empty cards_wins in
+  let wins_map =
+    List.fold_left (fun acc (id, win) -> M.add id win acc) M.empty cards_wins
+  in
   let get_values = get_scratched_card wins_map in
   let count_map = get_values M.empty 1 in
   let sum = M.fold (fun _ value acc -> acc + value) count_map 0 in
   sum
-;;
 
-
-let run (path: string) = 
+let run (path : string) =
   let input = Parse.read path in
-  let result = logic2 input in 
+  let result = logic2 input in
   print_int result
-;;
 
 (* Part 1 *)
 
-let test_input = "
-Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-"
-
+let test_input =
+  "\n\
+   Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
+   Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
+   Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n\
+   Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
+   Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
+   Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n"
 
 (* let%test_unit "logic" =   *)
 (*   let expected = 13 in *)
--- lib/day_5.ml
+++ lib/day_5.ml
@@ -4,217 +4,168 @@ open List
 
 (* data structures *)
 
-type transform = {
-  s: int;
-  e: int;
-  inc: int;
-}
-
-type stage = {
-  id: string;
-  transforms: transform list;
-}
-
+type transform = { s : int; e : int; inc : int }
+type stage = { id : string; transforms : transform list }
 
 (* parsing *)
 
-let get_seeds (line: string): int list = 
+let get_seeds (line : string) : int list =
   let parts = String.split_on_char ':' line in
-  let seeds = List.nth parts 1 
-              |> String.trim
-              |> String.split_on_char ' '
-              |> List.map int_of_string
-  in seeds
-;;
+  let seeds =
+    List.nth parts 1 |> String.trim |> String.split_on_char ' '
+    |> List.map int_of_string
+  in
+  seeds
 
-let get_seeds_ranges (line: string): (int* int) list = 
+let get_seeds_ranges (line : string) : (int * int) list =
   let parts = get_seeds line in
   (* group by 2 *)
-  let rec loop (parts: int list) (ranges: (int * int) list): (int * int) list = 
+  let rec loop (parts : int list) (ranges : (int * int) list) : (int * int) list
+      =
     match parts with
     | [] -> ranges
-    | start::range::rest -> 
-      let new_ranges = (start, start + range)::ranges in
-      loop rest new_ranges
+    | start :: range :: rest ->
+        let new_ranges = (start, start + range) :: ranges in
+        loop rest new_ranges
     | _ -> failwith "invalid input"
-  in loop parts []
-;;
+  in
+  loop parts []
 
-let get_transform (line: string): transform = 
+let get_transform (line : string) : transform =
   let parts = String.split_on_char ' ' line in
   let values = List.map int_of_string parts in
   let destination = List.nth values 0 in
   let s = List.nth values 1 in
   let range = List.nth values 2 in
-  {
-    s = s;
-    e = (s + range);
-    inc = destination - s;
-  }
-;;
+  { s; e = s + range; inc = destination - s }
 
-let get_reverse_tranform (line: string): transform = 
+let get_reverse_tranform (line : string) : transform =
   let parts = String.split_on_char ' ' line in
   let values = List.map int_of_string parts in
   let s = List.nth values 0 in
   let destination = List.nth values 1 in
   let range = List.nth values 2 in
-  {
-    s = s;
-    e = (s + range);
-    inc = destination - s;
-  }
-;;
+  { s; e = s + range; inc = destination - s }
 
-let get_stage (lines: string list): stage = 
+let get_stage (lines : string list) : stage =
   let id = List.nth lines 0 in
-  let transforms = List.tl lines
-                   |> List.map get_transform in
-  {
-    id = id;
-    transforms = transforms;
-  }
-;;
+  let transforms = List.tl lines |> List.map get_transform in
+  { id; transforms }
 
-let get_reverse_stage (lines: string list): stage = 
+let get_reverse_stage (lines : string list) : stage =
   let id = List.nth lines 0 in
-  let transforms = List.tl lines
-                   |> List.map get_reverse_tranform in
-  {
-    id = id;
-    transforms = transforms;
-  }
-;;
+  let transforms = List.tl lines |> List.map get_reverse_tranform in
+  { id; transforms }
 
-let split_by_empty_line (lines: string list): string list list = 
-  let rec loop (lines: string list) (stages: string list list): string list list = 
+let split_by_empty_line (lines : string list) : string list list =
+  let rec loop (lines : string list) (stages : string list list) :
+      string list list =
     match lines with
     | [] -> stages
-    | ""::rest -> loop rest ([]::stages)
-    | line::rest_lines -> 
-      let c_stage = List.hd stages in
-      let rest_stages = List.tl stages in 
-      let updated_stage = line::c_stage in
-      loop rest_lines (updated_stage::rest_stages)
-  in 
-  loop lines [[]]
-  |> List.map List.rev
-  |> List.rev
+    | "" :: rest -> loop rest ([] :: stages)
+    | line :: rest_lines ->
+        let c_stage = List.hd stages in
+        let rest_stages = List.tl stages in
+        let updated_stage = line :: c_stage in
+        loop rest_lines (updated_stage :: rest_stages)
+  in
+  loop lines [ [] ] |> List.map List.rev |> List.rev
 
 (* logic *)
 
-let transform (transforms: transform list) (origin: int): int = 
-  let rec loop (transforms: transform list) (origin: int): int = 
+let transform (transforms : transform list) (origin : int) : int =
+  let rec loop (transforms : transform list) (origin : int) : int =
     match transforms with
     | [] -> origin
-    | {s;e;inc}::rest -> 
-      if origin >= s && origin <= e then 
-        (origin + inc)
-      else 
-        loop rest origin
-  in loop transforms origin 
-;;
+    | { s; e; inc } :: rest ->
+        if origin >= s && origin <= e then origin + inc else loop rest origin
+  in
+  loop transforms origin
 
-let combine_stages (stages: stage list) (origin: int): int = 
-  let rec loop (stages: stage list) (origin: int): int = 
+let combine_stages (stages : stage list) (origin : int) : int =
+  let rec loop (stages : stage list) (origin : int) : int =
     match stages with
     | [] -> origin
-    | stage::rest -> 
-      let origin = transform stage.transforms origin in
-      loop rest origin
-  in loop stages origin
-;;
+    | stage :: rest ->
+        let origin = transform stage.transforms origin in
+        loop rest origin
+  in
+  loop stages origin
 
 (* main *)
 
-let logic (lines: string list): int  = 
+let logic (lines : string list) : int =
   (* parsing *)
   let seeds = get_seeds (List.hd lines) in
-  let stages = List.tl lines
-               |> List.tl
-               |> split_by_empty_line 
-               |> List.map get_stage in
+  let stages =
+    List.tl lines |> List.tl |> split_by_empty_line |> List.map get_stage
+  in
   (* logic *)
   (* !This is really amazing to be able to do just this! *)
   let transformation = combine_stages stages in
   let results = List.map transformation seeds in
-  List.fold_left min max_int results 
-;;
+  List.fold_left min max_int results
 
-let logic2 (lines: string list): int  = 
+let logic2 (lines : string list) : int =
   (* parsing *)
   let ranges = get_seeds_ranges (List.hd lines) in
-  let rev_stages = List.tl lines
-                   |> List.tl
-                   |> split_by_empty_line 
-                   |> List.rev
-                   |> List.map get_reverse_stage in
+  let rev_stages =
+    List.tl lines |> List.tl |> split_by_empty_line |> List.rev
+    |> List.map get_reverse_stage
+  in
 
   (* logic *)
   let transformation = combine_stages rev_stages in
-  let rec loop (i: int): int = 
+  let rec loop (i : int) : int =
     let result = transformation i in
-    let is_in_range = List.exists (fun (s, e) -> result >= s && result <= e) ranges in 
-    if is_in_range then
-      i
-    else if i > 100000000 then 
-      failwith "too big"
-    else 
-      loop (i + 1)
-  in 
+    let is_in_range =
+      List.exists (fun (s, e) -> result >= s && result <= e) ranges
+    in
+    if is_in_range then i
+    else if i > 100000000 then failwith "too big"
+    else loop (i + 1)
+  in
   loop 0
-;;
 
-let run (path: string) = 
+let run (path : string) =
   let input = Parse.read path in
-  let result = logic2 input in 
+  let result = logic2 input in
   print_int result
-;;
-
 
 (* tests - Part 1 *)
 
-let test_input = "
-seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4
-"
-
+let test_input =
+  "\n\
+   seeds: 79 14 55 13\n\n\
+   seed-to-soil map:\n\
+   50 98 2\n\
+   52 50 48\n\n\
+   soil-to-fertilizer map:\n\
+   0 15 37\n\
+   37 52 2\n\
+   39 0 15\n\n\
+   fertilizer-to-water map:\n\
+   49 53 8\n\
+   0 11 42\n\
+   42 0 7\n\
+   57 7 4\n\n\
+   water-to-light map:\n\
+   88 18 7\n\
+   18 25 70\n\n\
+   light-to-temperature map:\n\
+   45 77 23\n\
+   81 45 19\n\
+   68 64 13\n\n\
+   temperature-to-humidity map:\n\
+   0 69 1\n\
+   1 0 69\n\n\
+   humidity-to-location map:\n\
+   60 56 37\n\
+   56 93 4\n"
 
 (* let%test_unit "logic" =   *)
 (*   let expected = 35 in *)
 (*   [%test_eq: int] (logic (Parse.get_lines test_input)) expected *)
-
 
 (* let%test_unit "logic" =   *)
 (*   let real_input = Parse.read "../inputs/day_5.txt" in *)
@@ -222,7 +173,6 @@ humidity-to-location map:
 (*   [%test_eq: int] (logic (real_input)) expected *)
 
 (* tests - Part 2 *)
-
 
 (* let%test_unit "logic" =   *)
 (*   let expected = 46 in *)
