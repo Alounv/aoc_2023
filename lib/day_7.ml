39e3f5f451420cadf5b67289158c4b771f23ddd7
Alounv <34238160+Alounv@users.noreply.github.com>
2023-12-30 21:55:55 +0100

    Day 7 1

---
+++ lib/day_7.ml
@@ -0,0 +1,165 @@
+open Ppx_compare_lib.Builtin
+open Sexplib.Std
+open List
+
+(* utilities *)
+
+(* data structures *)
+
+type repartition = (char, int) Hashtbl.t
+
+type figures =
+  | FiveOfAKind
+  | FourOfAKind
+  | FullHouse
+  | ThreeOfAKind
+  | TwoPairs
+  | OnePair
+  | HighCard
+
+(* parsing *)
+
+let get_hand_bid (line : string) : char list * int =
+  let split = String.split_on_char ' ' line in
+  let hand = split |> hd |> String.to_seq |> List.of_seq in
+  let bid = split |> tl |> hd |> int_of_string in
+  (hand, bid)
+
+let get_repartition (hand : char list) : repartition =
+  let rec aux (hand : char list) (acc : repartition) : repartition =
+    match hand with
+    | [] -> acc
+    | h :: _ ->
+        let previous = Hashtbl.find_opt acc h |> Option.value ~default:0 in
+        Hashtbl.replace acc h (previous + 1);
+        aux (tl hand) acc
+  in
+  aux hand (Hashtbl.create 5)
+
+(* logic *)
+
+let get_highest_figure (repartition : repartition) : figures =
+  (* repartition |> Hashtbl.iter (fun char v -> print_int v; print_string " "; print_char char; print_endline ""); *)
+  let entries = repartition |> Hashtbl.to_seq |> List.of_seq in
+  let sorted_entries = List.sort (fun (_, v1) (_, v2) -> v2 - v1) entries in
+  let first = hd sorted_entries in
+
+  match snd first with
+  | 5 -> FiveOfAKind
+  | 4 -> FourOfAKind
+  | 3 -> (
+      match sorted_entries |> tl |> hd |> snd with
+      | 2 -> FullHouse
+      | _ -> ThreeOfAKind)
+  | 2 -> (
+      match sorted_entries |> tl |> hd |> snd with
+      | 2 -> TwoPairs
+      | _ -> OnePair)
+  | 1 -> HighCard
+  | _ -> failwith "invalid figure"
+
+let get_figure_value (figure : figures) : int =
+  match figure with
+  | FiveOfAKind -> 60
+  | FourOfAKind -> 50
+  | FullHouse -> 40
+  | ThreeOfAKind -> 30
+  | TwoPairs -> 20
+  | OnePair -> 10
+  | HighCard -> 0
+
+let get_card_value (card : char) : int =
+  match card with
+  | '2' -> 2
+  | '3' -> 3
+  | '4' -> 4
+  | '5' -> 5
+  | '6' -> 6
+  | '7' -> 7
+  | '8' -> 8
+  | '9' -> 9
+  | 'T' -> 10
+  | 'J' -> 11
+  | 'Q' -> 12
+  | 'K' -> 13
+  | 'A' -> 14
+  | _ -> failwith "invalid card"
+
+let get_hands_values (hands : (char list * int) list) :
+    (char list * int * int) list =
+  let rec aux (hands : (char list * int) list)
+      (acc : (char list * int * int) list) : (char list * int * int) list =
+    match hands with
+    | [] -> acc
+    | (hand, bid) :: tl ->
+        let repartition = get_repartition hand in
+        let figure = get_highest_figure repartition in
+        let value = get_figure_value figure in
+        aux tl ((hand, value, bid) :: acc)
+  in
+  aux hands []
+
+let compare_cards (char1 : char) (char2 : char) : int =
+  let card1 = char1 |> get_card_value in
+  let card2 = char2 |> get_card_value in
+  card2 - card1
+
+let compare_hands_by_cards (hand1 : char list) (hand2 : char list) : int =
+  let rec aux (hand1 : char list) (hand2 : char list) : int =
+    match (hand1, hand2) with
+    | [], [] -> 0
+    | h1 :: t1, h2 :: t2 -> if h1 = h2 then aux t1 t2 else compare_cards h1 h2
+    | _ -> failwith "invalid hands"
+  in
+  aux hand1 hand2
+
+let compare_hands (hand1 : char list * int * int)
+    (hand2 : char list * int * int) : int =
+  let hand1, value1, _ = hand1 in
+  let hand2, value2, _ = hand2 in
+  if value1 = value2 then compare_hands_by_cards hand1 hand2
+  else value2 - value1
+
+let add_index_to_bid (bids : int list) : (int * int) list =
+  let rec aux (bids : int list) (acc : (int * int) list) : (int * int) list =
+    match bids with [] -> acc | h :: t -> aux t ((h, length t) :: acc)
+  in
+  aux bids []
+
+(* main *)
+
+let logic (input : string list) : int =
+  let hands = map get_hand_bid input in
+  let hands_values = get_hands_values hands in
+  (* print_endline ""; *)
+  (* hands_values |> iter (fun (hand, value, bid) -> hand |> iter print_char; print_string " "; print_int value; print_string " "; print_int bid; print_endline "";); *)
+  (* print_endline ""; *)
+  let sorted_hands = List.sort compare_hands hands_values in
+  let sorted_bids = sorted_hands |> map (fun (_, _, bid) -> bid) in
+
+  let ranked_bids = add_index_to_bid sorted_bids in
+  let result =
+    ranked_bids
+    |> map (fun (bid, rank) -> bid * (rank + 1))
+    |> fold_left ( + ) 0
+  in
+
+  result
+
+let run (path : string) =
+  let input = Parse.read path in
+  let result = logic input in
+  print_int result
+
+(* tests - Part 1 *)
+
+let test_input = "\n32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483\n"
+
+let%test_unit "logic" =
+  let expected = 6440 in
+  [%test_eq: int] (logic (Parse.get_lines test_input)) expected
+
+let%test_unit "logic" =
+  let real_input = Parse.read "../inputs/day_7.txt" in
+  let expected = 248422077 in
+  [%test_eq: int] (logic real_input) expected
