ec04ae80cf7b236877a1544abd105b2ee53c2c5e
Aloun Vangkeosay <alounv@gmail.com>
2024-01-04 02:26:17 +0100

    Day 8 2

--- lib/day_8.ml
+++ lib/day_8.ml
@@ -8,6 +8,8 @@ open List
 
 type node_description = { name : string; left : string; right : string }
 type instruction = L | R
+type event = { start : int; period : int }
+type max = { max_start : int }
 
 (* parsing *)
 
@@ -49,16 +51,67 @@ let rec move (nodes : node_description list) (instructions : instruction list)
       | L -> move nodes instructions (index + 1) node.left
       | R -> move nodes instructions (index + 1) node.right)
 
+let rec moveToZ (nodes : node_description list)
+    (instructions : instruction list) (index : int) (current : string)
+    (first : int) : int * int =
+  if current.[2] = 'Z' && first > 0 then (first, index)
+  else
+    let first =
+      if first > 0 then first else if current.[2] = 'Z' then index else 0
+    in
+    let used_index = index mod List.length instructions in
+    let dir = List.nth instructions used_index in
+    let node = List.find (fun n -> n.name = current) nodes in
+    match dir with
+    | L -> moveToZ nodes instructions (index + 1) node.left first
+    | R -> moveToZ nodes instructions (index + 1) node.right first
+
+let get_first_and_second_index (nodes : node_description list)
+    (instructions : instruction list) (index : int) (current : string) :
+  int * int =
+  let first_index, second_index = moveToZ nodes instructions index current 0 in
+  (first_index, second_index)
+
+let get_starting_names (nodes : node_description list) : string list =
+  nodes |> List.map (fun n -> n.name) |> List.filter (fun n -> n.[2] = 'A')
+
 let logic (lines : string list) : int =
   let instructions = lines |> get_instructions in
   let nodes = lines |> get_nodes_list in
   move nodes instructions 0 "AAA"
 
+let gcd (a : int) (b : int) : int =
+  let rec loop (a : int) (b : int) : int =
+    if b = 0 then a else loop b (a mod b)
+  in
+  loop a b
+
+let lcm (a : int) (b : int) : int = a * b / gcd a b
+let lcm_list (l : int list) : int = List.fold_left (fun acc x -> lcm acc x) 1 l
+
+let get_first_simultaneous_index (events : event list) : int =
+  let periods = events |> List.map (fun e -> e.period) in
+  let lcm = lcm_list periods in
+  lcm
+
+let logic2 (lines : string list) : int =
+  let instructions = lines |> get_instructions in
+  let nodes = lines |> get_nodes_list in
+  let starting_names = nodes |> get_starting_names in
+  let paths =
+    starting_names
+    |> List.map (fun n -> get_first_and_second_index nodes instructions 0 n)
+  in
+  let events = List.map (fun (a, b) -> { start = a; period = b - a }) paths in
+  get_first_simultaneous_index events
+
+(* input *)
+
 (* main *)
 
 let run (path : string) =
   let input = Parse.read path in
-  let result = logic input in
+  let result = logic2 input in
   print_int result
 
 (* tests - Part 1 *)
@@ -76,3 +129,24 @@ let%test_unit "logic" =
   [%test_eq: int] (logic real_input) expected
 
 (* tests - Part 2 *)
+
+let test_input =
+  "\n\
+   LR\n\n\
+   11A = (11B, XXX)\n\
+   11B = (XXX, 11Z)\n\
+   11Z = (11B, XXX)\n\
+   22A = (22B, XXX)\n\
+   22B = (22C, 22C)\n\
+   22C = (22Z, 22Z)\n\
+   22Z = (22B, 22B)\n\
+   XXX = (XXX, XXX)\n"
+
+let%test_unit "logic" =
+  let expected = 6 in
+  [%test_eq: int] (logic2 (Parse.get_lines test_input)) expected
+
+let%test_unit "logic" =
+  let real_input = Parse.read "../inputs/day_8.txt" in
+  let expected = 20220305520997 in
+  [%test_eq: int] (logic2 real_input) expected
