5a79f427d8f3c0df0a21bbefdeba47894675101c
Alounv <34238160+Alounv@users.noreply.github.com>
2024-01-11 15:46:32 +0100

    Day 11 2

--- lib/day_11.ml
+++ lib/day_11.ml
@@ -6,13 +6,13 @@ open Utilities
 let empty = '.'
 
 type star = { name : int; coord : point }
+type pair = { a : star; b : star }
 
 (* input *)
 
 let test_input =
   Parse.get_lines
-    "\n\
-     ...#......\n\
+    "...#......\n\
      .......#..\n\
      #.........\n\
      ..........\n\
@@ -22,8 +22,6 @@ let test_input =
      ..........\n\
      .......#..\n\
      #...#.....\n"
-
-(* utilities *)
 
 (* logic *)
 
@@ -54,53 +52,76 @@ let get_empty_rows (map : map) : int list =
   in
   aux 0 []
 
-let expand_line (line : line) (to_double : int list) : line =
-  let max_x = Array.length line - 1 in
-  let rec aux (x : int) (acc : char list) =
-    if x > max_x then acc
-    else
-      let is_to_double = mem x to_double in
-      if is_to_double then aux (x + 1) (line.(x) :: line.(x) :: acc)
-      else aux (x + 1) (line.(x) :: acc)
-  in
-  let list = aux 0 [] in
-  list |> List.rev |> Array.of_list
-
-let expand_map (map : map) : map =
-  let empty_rows = get_empty_rows map in
-  let empty_columns = get_empty_columns map in
-  let max_y = Array.length map - 1 in
-
-  let rec aux (y : int) (acc : line list) =
-    if y > max_y then acc
-    else
-      let line = map.(y) in
-      let line = expand_line line empty_columns in
-      let is_to_double = mem y empty_rows in
-      if is_to_double then aux (y + 1) (line :: line :: acc)
-      else aux (y + 1) (line :: acc)
-  in
-
-  let new_list = aux 0 [] in
-  new_list |> List.rev |> Array.of_list
-
 let get_stars (map : map) : star list =
   let coords = get_coords map '#' in
   coords |> List.mapi (fun i coord -> { name = i; coord })
 
-(* logic 1 *)
+let get_distance (a : point) (b : point) : int =
+  let x = a.x - b.x |> abs in
+  let y = a.y - b.y |> abs in
+  x + y
 
-let logic (input : string list) =
-  let _map = map_from_strings input in
-  0
+let get_pairs (stars : star list) : pair list =
+  let rec aux (a : star) (stars : star list) (acc : pair list) =
+    match stars with
+    | [] -> acc
+    | b :: rest ->
+      let pair = { a; b } in
+      aux a rest (pair :: acc)
+  in
+  let rec aux2 (stars : star list) (acc : pair list) =
+    match stars with
+    | [] -> acc
+    | a :: rest ->
+      let pairs = aux a rest [] in
+      aux2 rest (pairs @ acc)
+  in
+  aux2 stars []
 
 (* logic 2 *)
+
+let count_expanses_between (expanded_rows : int list)
+    (expanded_columns : int list) (a : point) (b : point) : int =
+  let rows =
+    List.filter
+      (fun y -> (y > a.y && y < b.y) || (y > b.y && y < a.y))
+      expanded_rows
+    |> List.length
+  in
+  let columns =
+    List.filter
+      (fun x -> (x > a.x && x < b.x) || (x > b.x && x < a.x))
+      expanded_columns
+    |> List.length
+  in
+  rows + columns
+
+let get_distance2 (factor : int) (expanded_rows : int list)
+    (expanded_columns : int list) (a : point) (b : point) : int =
+  let distance = get_distance a b in
+
+  let count = count_expanses_between expanded_rows expanded_columns in
+  let expanses = count a b in
+  let new_distance = distance + (expanses * (factor - 1)) in
+  new_distance
+
+let logic (input : string list) (factor : int) =
+  let map = map_from_strings input in
+  let empty_rows = get_empty_rows map in
+  let empty_columns = get_empty_columns map in
+  let stars = get_stars map in
+  let pairs = get_pairs stars in
+  let get_d = get_distance2 factor empty_rows empty_columns in
+  let distances = List.map (fun { a; b } -> get_d a.coord b.coord) pairs in
+
+  let sum = List.fold_left ( + ) 0 distances in
+  sum
 
 (* main *)
 
 let run (_path : string) =
   (* let input = Parse.read path in *)
-  let result = logic test_input in
+  let result = logic test_input 2 in
   print_int result
 
 (* tests - Part 1 *)
@@ -113,36 +134,28 @@ let%test_unit "get_empty_rows" =
   let map = map_from_strings test_input in
   [%test_eq: int list] (get_empty_rows map) [ 7; 3 ]
 
-let%test_unit "expand_line" =
-  let line = [| '.'; '.'; 'A'; '#'; '.'; 'B'; '.'; '.'; 'C'; '.' |] in
-  let to_double = [ 8; 5; 2 ] in
-  let expected =
-    [| '.'; '.'; 'A'; 'A'; '#'; '.'; 'B'; 'B'; '.'; '.'; 'C'; 'C'; '.' |]
-  in
-  let result = expand_line line to_double in
-  let result_list = Array.to_list result in
-  let expected_list = Array.to_list expected in
-  [%test_eq: char list] result_list expected_list
+let%test_unit "get_distance" =
+  let a = { x = 0; y = 0 } in
+  let b = { x = 4; y = 5 } in
+  let expected = 9 in
+  [%test_eq: int] (get_distance a b) expected
 
-let%test_unit "expand_map" =
-  let map = map_from_strings test_input in
-  let result = expand_map map in
-  let string_result = string_from_map result in
-  let expected =
-    "....#........\n\
-     .........#...\n\
-     #............\n\
-     .............\n\
-     .............\n\
-     ........#....\n\
-     .#...........\n\
-     ............#\n\
-     .............\n\
-     .............\n\
-     .........#...\n\
-     #....#......."
-  in
-  [%test_eq: string] string_result expected
+let%test_unit "get_distance" =
+  let a = { x = 0; y = 0 } in
+  let b = { x = 4; y = 5 } in
+  let expected = 9 in
+  [%test_eq: int] (get_distance b a) expected
+
+let%test_unit "logic" =
+  let expected = 374 in
+  [%test_eq: int] (logic test_input 2) expected
+
+let%test_unit "logic" =
+  let real_input = Parse.read "../inputs/day_11.txt" in
+  let expected = 9605127 in
+  [%test_eq: int] (logic real_input 2) expected
+
+(* tests - Part 2 *)
 
 let%test_unit "get_stars" =
   let map = map_from_strings test_input in
@@ -166,13 +179,47 @@ let%test_unit "get_stars" =
   in
   [%test_eq: string] result_string expected
 
-(* let%test_unit "logic" = *)
-(*   let expected = 8 in *)
-(*   [%test_eq: int] (logic test_input) expected *)
+let%test_unit "count_expanses_between_points" =
+  let a = { x = 1; y = 1 } in
+  let b = { x = 6; y = 4 } in
+  let expanded_columns = [ 8; 5; 2 ] in
+  let expanded_rows = [ 7; 3 ] in
+  let expected = 3 in
+  [%test_eq: int]
+    (count_expanses_between expanded_rows expanded_columns a b)
+    expected
 
-(* let%test_unit "logic" = *)
-(*   let real_input = Parse.read "../inputs/day_11.txt" in *)
-(*   let expected = 7097 in *)
-(*   [%test_eq: int] (logic real_input) expected *)
+let%test_unit "count_expanses_between_points" =
+  let a = { x = 1; y = 1 } in
+  let b = { x = 6; y = 4 } in
+  let expanded_columns = [ 8; 5; 2 ] in
+  let expanded_rows = [ 7; 3 ] in
+  let expected = 3 in
+  [%test_eq: int]
+    (count_expanses_between expanded_rows expanded_columns b a)
+    expected
 
-(* tests - Part 2 *)
+let%test_unit "get_distance2" =
+  let a = { x = 1; y = 1 } in
+  let b = { x = 6; y = 4 } in
+  let expanded_columns = [ 8; 5; 2 ] in
+  let expanded_rows = [ 7; 3 ] in
+  let expected = 8 in
+  [%test_eq: int] (get_distance2 1 expanded_rows expanded_columns a b) expected
+
+let%test_unit "logic" =
+  let expected = 374 in
+  [%test_eq: int] (logic test_input 2) expected
+
+let%test_unit "logic" =
+  let expected = 1030 in
+  [%test_eq: int] (logic test_input 10) expected
+
+let%test_unit "logic" =
+  let expected = 8410 in
+  [%test_eq: int] (logic test_input 100) expected
+
+let%test_unit "logic" =
+  let real_input = Parse.read "../inputs/day_11.txt" in
+  let expected = 458191688761 in
+  [%test_eq: int] (logic real_input 1000000) expected
